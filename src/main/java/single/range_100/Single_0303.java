7ea74662554308f86b134f2c431f5b6f2bb244c4
yancy <yancy2966@163.com>
2022-03-07 23:54:24 +0800

    add : Single

---
+++ src/main/java/single/range_100/Single_0303.java
@@ -0,0 +1,37 @@
+package single.range_100;
+
+/**
+ * 303. 区域和检索-数组不可变
+ *
+ * @author yancy
+ * @version 1.0
+ * @since 2022/3/7
+ */
+public class Single_0303 {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{-2,0,3,-5,2,-1};
+        NumArray numArray = new Single_0303().new NumArray(nums);
+        System.out.println(numArray.sumRange(0,2));
+        System.out.println(numArray.sumRange(2,5));
+
+
+    }
+
+    class NumArray {
+
+        int[] sums;
+        public NumArray(int[] nums) {
+            int n = nums.length;
+            sums = new int[n+1];
+            for (int i = 0; i < n; i++) {
+                sums[i+1] = sums[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return sums[right+1] - sums[left];
+        }
+    }
+}
---
+++ src/main/java/single/range_100/Single_0349.java
@@ -0,0 +1,45 @@
+package single.range_100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 349. 两个数组的交集
+ *
+ * @author yancy
+ * @version 1.0
+ * @since 2022/3/7
+ */
+public class Single_0349 {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{4,9,5};
+        int[] nums2 = new int[]{9,4,9,8,4};
+        int[] nums3 = new Single_0349().intersection(nums,nums2);
+        for (int i : nums3) {
+            System.out.print(i + ",");
+        }
+    }
+
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set02 = new HashSet<>();
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                set02.add(i);
+            }
+        }
+        int[] result = new int[set02.size()];
+        int i = 0;
+        for (int j : set02) {
+            result[i++] = j;
+        }
+        return result;
+    }
+
+}
