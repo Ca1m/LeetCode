dad45eef3bc4088ba1b84ba1bc2efdb1468f3ee8
yancy <yancy2966@163.com>
2022-03-09 23:34:38 +0800

    add

---
+++ src/main/java/single/range_500/Single_0598.java
@@ -0,0 +1,29 @@
+package single.range_500;
+
+/**
+ * 598. 范围求和 2
+ *
+ * @author yancy
+ * @version 1.0
+ * @since 2022/3/9
+ */
+public class Single_0598 {
+
+
+    public static void main(String[] args) {
+
+        int[][] ops = new int[][] {{}};
+
+
+    }
+
+    public int maxCount(int m, int n, int[][] ops) {
+
+        for (int[] nums : ops) {
+            m = Math.min(m, nums[0]);
+            m = Math.min(m, nums[1]);
+        }
+        return m * n;
+    }
+
+}
